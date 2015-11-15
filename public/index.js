196a4d34badf0b4963e4aded1641bfae33e76fef
Dominique Miller <dominique.p.miller@gmail.com>
2015-11-14 19:41:44 -0500

    initial

---
+++ public/index.js
@@ -0,0 +1,19 @@
+$(document).on('ready', function(){
+  $('.projects').click(function(){
+    $('.ruby-projects').bPopup({
+      speed: 650,
+      transition: 'slideIn',
+      transitionClose: 'slideBack'
+    });
+  });
+  $('.projects-ii').click(function(){
+    $('.rails-projects').bPopup({
+      speed: 650,
+      transition: 'slideIn',
+      transitionClose: 'slideBack'
+    });
+  });
+  $('.contact-info-top').click(function(){
+    $('.contact-info').bPopup();
+  });
+});
